9445630f84d1190e6b1cacaa978b4c051c92c544
ilyamiro <ilyamiro.work@gmail.com>
2026-04-09 21:40:00 +0200

    Update version

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # ==============================================================================
 # Script Versioning & Initialization
 # ==============================================================================
-DOTS_VERSION="1.0.16-1"
+DOTS_VERSION="1.0.18"
 VERSION_FILE="$HOME/.local/state/imperative-dots-version"
 
 # Global Variables & Initial States (Defaults)
